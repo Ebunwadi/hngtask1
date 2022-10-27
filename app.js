de71e0a09f0f2fed7e9840ea6624d0741e3480ca
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-10-27 11:32:25 +0100

    modify app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({
+    res.status(201).send({
         slackUsername: 'ebube', 
         backend: true, 
         age: 27, 
